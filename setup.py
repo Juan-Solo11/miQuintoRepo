1da55275069e89f4df5a85530ba956ba78572c1a
Juan Monroy <juan.monroy22.jml@gmail.com>
2025-03-11 22:33:53 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miQuintoRepo",                            # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Juan Monroy",                           # Tu nombre
+    author_email="juan.monroy22.jml@gmail.com",     # Tu correo electrónico
+    url="https://github.com/juan-solo11/miquintorepo",     # URL del proyecto
+)
